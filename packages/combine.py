66ec27b41cb57da31052806b08223f6b723a0ca2
Uncle-Yuanl <1097304891@qq.com>
2021-03-16 19:28:12 +0800

    add cluster

--- packages/combine.py
+++ packages/combine.py
@@ -5,9 +5,9 @@ import pandas as pd
 import os
 import re
 import subprocess
-
-lexicon = pd.read_excel('./lexicon/词云0303-综合词库_全部_2021-03-10 19-38-48.xlsx', usecols=['TagName', 'Keywords'])
-lexicon = lexicon[lexicon['TagName'].str.startswith('需求')]
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import AgglomerativeClustering
 
 
 class Combine():
@@ -47,6 +47,9 @@ class Combine():
         df = pd.read_csv(abpath, error_bad_lines=False, dtype=str)
         if "ATTR" in df.columns and "attr" not in df.columns:
             df['attr'] = df["ATTR"]
+        df = df.dropna(subset=['attr'])
+        if len(df) == 0:
+            raise Exception("Please use comprehensive lexicon to label the data !")
         return df
 
     def __func(self, tags):
@@ -83,35 +86,7 @@ class Combine():
                         ls.append("需求." + b.split('.')[-1] + '_' + w.split('.')[-1] + '_' + c.split('.')[-1])
         return '|'.join(ls)
 
-    def label(self, df):
-        """use lexicon to label the df
-        this df should have the filed 'xuqiu' and not have field 'tags'
-        """
-        print('Start labelling......')
-        for _, rowd in df.iterrows():
-            attr = []
-            for _, rowl in lexicon.iterrows():
-                tmp = re.findall(re.escape(rowl['Keywords']), rowd['xuqiu'])
-                if tmp:
-                    attr.extend(tmp)
-            rowd['attr_new'] = '|'.join(attr)
-        print('Labelling completed......')
-        return df
-
-    def _combineM1(self, df):
-        """combine after cluster
-        """
-        if 'attr_new' not in df.columns:
-            raise Exception("Labelling failded......")
-        df['attr_new'] = df['attr_new'].astype('str')
-        df = df[['content', 'attr_new']]
-        df = df.drop_duplicates()
-        total = '|'.join(list(df[df['attr_new'].str.contains('问题期许')]['attr_new'].apply(self.__func))).split('|')
-        df1 = pd.DataFrame(collections.Counter(total).most_common())
-        df1.columns = ['组合需求', 'count']
-        return df1
-
-    def combineM2(self, df):
+    def combine(self, df):
         """combine after having tags
         """
         df.attr = df.attr.astype('str')
@@ -121,3 +96,69 @@ class Combine():
         df2 = pd.DataFrame(collections.Counter(total).most_common())
         df2.columns = ['组合需求', 'count']
         return df2
+
+    def cluster(self, filename, n_clusters, df=None, tofile=False):
+        """return result of cluster with given file or df
+        """
+        path = '/mnt/disk2/data/YuanHAO/对应关系应用/files/{}'.format(filename)
+        if not os.path.exists(path) and not df:
+            raise Exception("Please upload cluster file to ./files folder OR specify the df")
+        if not filename:
+            ws = df["组合需求"].tolist()
+        else:
+            ws = pd.read_excel(path, header=None)[0].tolist()
+
+        with codecs.open('/mnt/disk2/data/YuanHAO/对应关系应用/tongyong.pickle', "rb") as f:
+            VECTOR = pickle.load(f)
+        W_norm = VECTOR['W_norm']
+        vocab = VECTOR['vocab']
+        wsvec = []
+        newws = []
+        print('****不在词典里的词有****')
+
+        _fix = '的|了|有点|多|没有|长了|不|的|特别|不错|好|好处|不|没有|无|不会|可以|一直|不用|超级|慢慢|会'.split('|')
+        for x in ws:
+            cut_x = x
+            for _ in _fix:
+                cut_x = cut_x.strip(_)
+
+            if cut_x in vocab:
+                vec = W_norm[vocab[cut_x]]
+                if sum(np.isnan(vec)) != 0 or sum(pd.DataFrame(vec)[0].apply(math.isinf)) != 0:
+                    print(x, ': nan or inf')
+                    continue
+                wsvec.append(vec)
+                newws.append(x)
+            else:
+                cut_x = list(jieba.cut(cut_x))
+                tmp = [W_norm[vocab[w]] for w in cut_x if w in vocab]
+                if len(tmp) == 0:
+                    print(x)
+                else:
+                    wsvec.append(sum(tmp) / len(tmp))
+                    newws.append(x)
+
+        n_clusters = n_clusters
+        kmeans_model = KMeans(n_clusters=n_clusters, random_state=1).fit(wsvec)
+        labels = kmeans_model.labels_
+
+        outputwords = []
+        outputvecs = []
+        for i in range(n_clusters):
+            tmpword, tmpvec = [], []
+            print('\nClass {}'.format(i))
+            for j in range(len(wsvec)):
+                if labels[j] == i:
+                    if not df:
+                        print(newws[j], end=',')
+                    tmpword.append(newws[j])
+                    tmpvec.append(wsvec[j])
+            outputvecs.append(tmpvec)
+            outputwords.append(tmpword)
+        if tofile:
+            respath = '/mnt/disk2/data/YuanHAO/对应关系应用/files/聚类结果.txt'
+            with open(respath, 'w', encoding='utf-8') as f:
+                for cls, wl in enumerate(test):
+                    f.write("class: {}".format(cls))
+                    for word in wl:
+                        f.write(word)
