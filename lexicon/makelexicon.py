393ee5d9687ef4ae35f5a6a0dcc098c0a94cca78
Uncle-Yuanl <1097304891@qq.com>
2021-03-17 18:07:41 +0800

    fix duplicates

--- lexicon/makelexicon.py
+++ lexicon/makelexicon.py
@@ -19,6 +19,12 @@ if __name__ == '__main__':
 
     # 拼接
     dfres = pd.concat([df_qixu, df_res, df_buwei, df_chengdu])
+    dfres.drop_duplicates(subset=['TagName'])
 
-    # 写入文件
-    dfres.to_excel('./final_lexicon.xlsx', index=False, encoding='utf-8')
+    # 写入文件，两个sheet
+    # dfres.to_excel('./final_lexicon.xlsx', index=False, encoding='utf-8')
+    writer = pd.ExcelWriter('./final_lexicon.xlsx', engine='xlsxwriter')
+    dfres.to_excel(writer, sheet_name='叶子词库', index=False, encoding='utf-8')
+    dfweidu = pd.read_excel('./奶粉TA词库 - 更新0223-全all.xlsx', sheet_name='生成维度')
+    dfweidu.to_excel(writer, sheet_name='生成维度', index=False, encoding='utf-8')
+    writer.save()
